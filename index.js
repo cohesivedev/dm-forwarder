169f29297e9a3a7ecefc18583bdc8e58c67705dc
jsyang <jsyang.ca@gmail.com>
2019-03-03 02:54:14 +0000

    log messages in heroku log as well add better logging messages as well as content-length truncation and limits ensure better log messages are shown for request processing fails add better readme

--- index.js
+++ index.js
@@ -1,19 +1,38 @@
 const http    = require('http');
 const Twitter = require('twitter-lite');
 
-const {PORT, REFERER, RECEPIENT_ID, CONSUMERKEY, CONSUMERSECRET, TOKENKEY, TOKENSECRET, REDIRECT} = process.env;
+const {
+          PORT, REFERER, RECIPIENT_ID,
+          CONSUMERKEY, CONSUMERSECRET,
+          TOKENKEY, TOKENSECRET,
+          REDIRECT, MAX_BASE64_LENGTH
+      } = process.env;
 
 const FORM_URLENCODED = 'application/x-www-form-urlencoded';
 
 function handlePOST(req) {
     return new Promise((resolve, reject) => {
-        if (req.headers['content-type'] === FORM_URLENCODED) {
+        const size = parseFloat(req.headers['content-length']);
+        const MAX_SIZE = parseFloat(MAX_BASE64_LENGTH);
+        const isWithinSize  = size <= MAX_SIZE;
+        const isCorrectType = req.headers['content-type'] === FORM_URLENCODED;
+
+        if (isCorrectType && isWithinSize) {
             let body = '';
 
-            req.on('data', chunk => (body += chunk.toString()));
+            req.on('data', chunk => {
+                // max length should be at most 2 * message char count
+                if (body.length < MAX_SIZE) {
+                    body += chunk.toString();
+                }
+            });
             req.on('end', () => resolve(body));
         } else {
-            reject('Not form type POST request!');
+            reject([
+                'Failed to process request due to:',
+                !isCorrectType && '- content being incorrectly typed',
+                !isWithinSize && ( '- content being over max length ' + req.headers['content-length']+'/'+MAX_BASE64_LENGTH)
+            ].filter(Boolean).join('\n'));
         }
     });
 }
@@ -43,31 +62,40 @@ function sendDM(text) {
             "event": {
                 "type":           "message_create",
                 "message_create": {
-                    "target":       {"recipient_id": RECEPIENT_ID},
+                    "target":       {"recipient_id": RECIPIENT_ID},
                     "message_data": {"text": text}
                 }
             }
         }
     )
-        .then(() => console.log(`DM sent! ${Date.now()}`))
+        .then(() => console.log(`DM sent:\n\n${text}\n\n`))
         .catch(e => console.error(e));
+}
+
+function respondWithThanks(res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(`
+<html>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="refresh" content="2;url=${REDIRECT}"/>
+    <link rel="icon" href="data:,">
+    <span style="position: absolute; top:50%; left:50%; transform: translate(-50%, -50%);">Thank you!<br>Now returning to <a href="${REFERER}">${REFERER}</a>...</span>
+</html>
+`);
 }
 
 http.createServer((req, res) => {
     const {headers, method} = req;
-    console.log(headers.referer);
-    if (method === 'POST' && headers.referer === REFERER) {
+
+    if (method === 'POST' && headers.referer.indexOf(REFERER)===0) {
+        console.log(`Received from web form!`);
         handlePOST(req)
             .then(getJSONFromFormBody)
             .then(data => sendDM(data.msg))
-            .catch(e => console.error(e));
+            .catch(e => console.error(e))
+            .finally(() => respondWithThanks(res));
+    } else {
+        console.log('Req did not pass verification checks!');
+        respondWithThanks(res);
     }
-
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(`
-<html>
-    <meta http-equiv="refresh" content="2;url=${REDIRECT}"/>
-    <span style="position: absolute; top:50%; left:50%; transform: translate(-50%, -50%);">Thank you.</span>
-</html>
-`);
 }).listen(PORT);
---
+++ readme.md
@@ -0,0 +1,19 @@
+# dm-forwarder
+
+Single endpoint API for forwarding form data as Twitter DMs:
+1. HTML form from the `REFERER` URL sends its formData with parameter `msg` as a base64 encoded message
+2. `dm-forwarder` receives the request and decodes it to get the message text
+3. Then on behalf of the user who has given their keys for the config, it sends this as a DM to the Twitter 
+user under `RECIPIENT_ID`
+4. Message arrives for the recipient in their Twitter inbox
+
+## Setup
+ 
+1. Create a Heroku app
+2. Configure Heroku "Config Vars" (environment variables):
+    - Twitter API keys and secrets x 4
+    - RECIPIENT_ID should be the [Twitter ID](http://gettwitterid.com)
+    - REDIRECT should be a URL that end user gets sent back to after submission
+    - REFERER restricts where the endpoint can be reached from
+    - MAX_BASE64_LENGTH restricts how large a message can be before it gets rejected
+3. Either manually deploy or automatically deployed when new commits arrive into the codebase.
